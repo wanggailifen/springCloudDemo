cd9e993eba1b4e699c1292c1ef7d2d878b675858
18202729318@163.com <18202729318@163.com>
2019-09-02 17:37:02 +0800

    Initial commit

---
+++ springcloud-api/src/main/java/com/tonglc/demo/entity/Dept.java
@@ -0,0 +1,32 @@
+package com.tonglc.demo.entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+
+@NoArgsConstructor
+@Data
+@Accessors(chain=true)  //表示可以链式编程
+public class Dept implements Serializable// entity --orm--- db_table
+{
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	
+	public Dept(String dname)
+	{
+		super();
+		this.dname = dname;
+	}
+
+	public static void main(String[] args) {
+		new Dept().setDeptno((long) 12).setDname("test").setDb_source("XXXXXX");
+	}
+	
+}
