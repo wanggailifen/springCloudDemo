fe344a6b2ef7fd7d6f11e2aeaf5f39cfcf9bb1b1
18202729318@163.com <18202729318@163.com>
2019-09-03 15:49:58 +0800

    服务注册与发现

---
+++ springcloud-provider-dept-8001/src/main/java/com/tonglc/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,21 @@
+package com.tonglc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@SpringBootApplication
+@EnableDiscoveryClient //服务发现
+public class DeptProvider8001_App
+{
+  public static void main(String[] args)
+  {
+   SpringApplication.run(DeptProvider8001_App.class, args);
+  }
+}
+ 
